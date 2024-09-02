09619a2bd52032ebfb5c6b8acef3e0743bfa3202
windlil <windlil@outlook.com>
2024-09-02 11:55:01 +0800

    refactor: fixed the component re-rendering issue

--- src/core/render/renderComponents.tsx
+++ src/core/render/renderComponents.tsx
@@ -1,30 +1,36 @@
-import { createElement } from 'react'
+import { createElement, FC, memo } from 'react'
 import { componentMap } from '../componentMap'
 import useComponentsStore, { ComponentItem } from '@/stores/components'
 
-export const renderComponents = (components: ComponentItem[]) => {
-  const curComponent = useComponentsStore(state => state.curComponent)
+const Component:FC<{
+  component: ComponentItem
+}> = memo(({ component }) => {
   const setCurComponent = useComponentsStore(state => state.setCurComponent)
 
   const handleClickComponent = (component: ComponentItem) => {
     setCurComponent(component.id)
   }
 
+  return (
+    <div
+      className='absolute border border-dashed border-transparent hover:border-[#44526bc8]'
+      style={{
+        left: component.config.x,
+        top: component.config.y,
+        zIndex: component.config.z,
+      }}
+      key={Math.random()}
+      onClick={() => handleClickComponent(component)}
+    >
+      {createElement((componentMap as any)[component.name], {config: component.config})}
+    </div>
+  )
+})
+
+export const renderComponents = (components: ComponentItem[]) => {
   return components.map((component) => {
     return (
-      <div
-        className='absolute border border-dashed border-transparent hover:border-[#44526bc8]'
-        style={{
-          left: component.x,
-          top: component.y,
-          zIndex: component.z,
-          borderColor: curComponent?.id === component.id ? '#3c7cec' : ''
-        }}
-        key={Math.random()}
-        onClick={() => handleClickComponent(component)}
-      >
-        {createElement((componentMap as any)[component.name], {config: component.config})}
-      </div>
+      <Component component={component} />
     )
   })
 }
