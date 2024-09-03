e9f3b6c1c345016fa52b8dc58b93b728abc73752
windlil <windlil@outlook.com>
2024-09-03 17:32:45 +0800

    feat: update hooks

--- src/hooks/useCanvasDrop.ts
+++ src/hooks/useCanvasDrop.ts
@@ -1,35 +1,42 @@
 import { useDrop } from 'react-dnd'
 import useComponentsStore from '@/stores/components'
 import { ConfigMap } from '@/core/defaultConfig/ConfigMap'
-import { RefObject } from 'react'
+import { RefObject, useCallback } from 'react'
+import useCanvasStore from '@/stores/canvas'
+
+const positionToFixed = (position: number) => {
+  return Number(position.toFixed(0))
+}
 
 const useCanvasDrop = (canvasRef: RefObject<HTMLDivElement>) => {
   const addComponent = useComponentsStore(state => state.addComponent)
+  const scale = useCanvasStore(state => state.scale)
+
+  const handleDrop = useCallback((item: any, monitor: any) => {
+    const { top, left } = canvasRef.current!.getBoundingClientRect()
+    const offsetX = monitor.getClientOffset()!.x - left
+    const offsetY = monitor.getClientOffset()!.y - top
+    
+    const component: {
+      id: string
+      name: string,
+      config: any
+    } = {
+      id: String(Math.random()),
+      name: item.name,
+      config: {
+        ...ConfigMap[item.name],
+        x: positionToFixed(offsetX * (1 / scale)),
+        y: positionToFixed(offsetY * (1 / scale))
+      }
+    }
+    addComponent(component)
+  }, [scale, addComponent])
 
   const [, drop] = useDrop(() => ({
     accept: 'component',
-    drop: (item: any, monitor) => {
-      const { top, left } = canvasRef.current!.getBoundingClientRect()
-      const offsetX = monitor.getClientOffset()!.x - left
-      const offsetY = monitor.getClientOffset()!.y - top
-      console.log(top, left, offsetX, offsetY)
-      const component: {
-        id: string
-        name: string,
-        config: any
-      } = {
-        id: String(Math.random()),
-        name: item.name,
-        config: {
-          ...ConfigMap[item.name],
-          x: offsetX,
-          y: offsetY
-        }
-      }
-
-      addComponent(component)
-    }
-  }))
+    drop: handleDrop
+  }), [handleDrop, scale])
 
   return {
     drop
